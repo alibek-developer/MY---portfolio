2dcfa9cd13f7b792352b447cd63aa3b39cba95c5
alibek-developer <a1ibekdew0@gmail.com>
2026-03-13 14:53:29 +0500

    added

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,16 @@
 import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
-    images: {
-        domains: ["via.placeholder.com"],
-    },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'via.placeholder.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
